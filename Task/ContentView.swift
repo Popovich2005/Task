12c3751545d223905a90fbeb5136e5e9d26c9dd6
Popovich2005 <popich2005@mail.ru>
2024-06-20 10:35:38 +0300

    Initial Commit

---
+++ Task/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Task
+//
+//  Created by Алексей Попов on 20.06.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Task/TaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskApp.swift
+//  Task
+//
+//  Created by Алексей Попов on 20.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct TaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
